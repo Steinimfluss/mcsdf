bd7670b4ddc5c10fbef0d6ac37ad7ff4488d9b87
Steinimfluss <tom.p.kuehner@outlook.de>
2026-04-05 17:57:12 +0200

    Initial commit

---
+++ src/client/java/io/github/mcsdf/client/McsdfClient.java
@@ -0,0 +1,10 @@
+package io.github.mcsdf.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McsdfClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
---
+++ src/main/java/io/github/mcsdf/Mcsdf.java
@@ -0,0 +1,12 @@
+package io.github.mcsdf;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mcsdf implements ModInitializer {
+    public static final String MOD_ID = "mcsdf";
+
+    @Override
+    public void onInitialize() {
+    	
+    }
+}
